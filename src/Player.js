020274e33c9b59f2d79a477980205693eee33434
otherchris <chriscaragianis@gmail.com>
2019-06-30 13:36:30 -0400

    initial commit, suuuper basic controls

---
+++ src/Player.js
@@ -0,0 +1,42 @@
+class Player {
+  constructor(params) {
+    const {type, frequency, duration} = params;
+
+    const context = new (window.AudioContext) || window.webkitAudioContext();
+    const osc = context.createOscillator();
+    const gainControl = context.createGain();
+
+    osc.type = type;
+    gainControl.gain.value = 0;
+    osc.connect(gainControl);
+    gainControl.connect(context.destination);
+
+    osc.start();
+
+    this.osc = osc;
+    this.gainControl = gainControl;
+    this.context = context;
+  }
+
+  play(frequency, duration) {
+    this.osc.frequency.setValueAtTime(frequency, this.context.currentTime);
+    this.gainControl.gain.value = 1;
+    setTimeout(() => {this.gainControl.gain.value = 0;}, duration);
+  }
+
+  on() {
+    this.gainControl.gain.value = 1;
+  }
+
+  off() {
+    this.gainControl.gain.value = 0;
+  }
+
+  setFreq(freq) {
+    this.osc.frequency.setValueAtTime(freq, this.context.currentTime);
+  }
+}
+
+const defaultPlayer = new Player({type: 'sine'});
+const sawPlayer = new Player({type: 'sawtooth'});
+
